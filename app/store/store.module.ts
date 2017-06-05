78feb9a69b687adf0fda9444928006db28031bca
Sean <seon2you@hotmail.com>
2017-06-04 23:22:16 -0700

    cart

--- app/store/store.module.ts
+++ app/store/store.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
 import { CounterDirective } from "./counter.directive";
+import { CartSummaryComponent } from "./cartSummary.component";
 
 @NgModule({
     imports: [BrowserModule, FormsModule, ModelModule],
-    declarations: [StoreComponent, CounterDirective],
+    declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
     exports: [StoreComponent]
 })
 
