00227a63c49dd29f531c7bc4d758f92c7fa02986
Sean <seon2you@hotmail.com>
2017-06-04 17:28:52 -0700

    start sport store app

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app",
+    // template: `
+    //     <div class="bg-success p-a-1 text-xs-center">
+    //         This is SportStore
+    //     </div>
+    // `
+    template: "<store></store>"
+})
+
+export class AppComponent { }
