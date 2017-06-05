abafc9af778917fdb21987ce91d5db41d1afa584
Sean <seon2you@hotmail.com>
2017-06-05 14:05:03 -0700

    admin

--- app/app.module.ts
+++ app/app.module.ts
@@ -25,6 +25,11 @@ import { StoreFirstGuard } from "./storeFirst.guard";
                 path: "checkout", component: CheckoutComponent,
                 canActivate: [StoreFirstGuard]
             },
+            {
+                path: "admin",
+                loadChildren: "app/admin/admin.module#AdminModule",
+                canActivate: [StoreFirstGuard]
+            },
             { 
                 path: "**", redirectTo: "/store" }
         ])
